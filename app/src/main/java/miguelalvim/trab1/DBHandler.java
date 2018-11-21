20ea8c1440f5f6fcfd6b6d2e2d75a82b910dad0e
YannHyaric <ylhalmeida@gmail.com>
2018-11-21 21:25:13 -0200

    adição de dados iniciais

--- app/src/main/java/miguelalvim/trab1/DBHandler.java
+++ app/src/main/java/miguelalvim/trab1/DBHandler.java
@@ -1,6 +1,7 @@
 package miguelalvim.trab1;
 
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,15 +16,30 @@ public class DBHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
         db.execSQL("PRAGMA foreign_keys = ON;");
         db.execSQL(
                 "CREATE TABLE pessoa (id INTEGER PRIMARY KEY AUTOINCREMENT," +
                         "name TEXT, cpf TEXT, email TEXT)"
         );
+
+        ContentValues vals = new ContentValues();
+        vals.put("email", "Jose@ice.com");
+        vals.put("cpf", "12345678901");
+        vals.put("name", "Jose");
+        db.insert("pessoa", null, vals);
+
         db.execSQL(
                 "CREATE TABLE evento (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         "titulo TEXT, dia TEXT, hora TEXT, facilitador TEXT, descricao TEXT)"
         );
+        vals.clear();
+        vals.put("dia", "12/23");
+        vals.put("descricao", "alguma coisa");
+        vals.put("titulo", "evento1");
+        vals.put("hora", "13:09");
+        vals.put("facilitador", "ze das cove");
+        db.insert("evento", null, vals);
         db.execSQL(
                 "CREATE TABLE pessoaevento (id_pessoa INTEGER, id_evento INTEGER, " +
                         "FOREIGN KEY(id_pessoa) REFERENCES pessoa(id)," +
